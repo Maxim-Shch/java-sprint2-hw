99179bbf30b11ea440c59a886aa3e5feb7109271
Maxim-Shch <jiimpopo007@gmail.com>
2023-08-09 22:16:08 +0300

    Исправлено замечание о инициализации YearlyReport.

--- src/Main.java
+++ src/Main.java
@@ -2,54 +2,50 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ReportReader reportReader = new ReportReader();
         ReportCompare reportCompare = new ReportCompare();
         InformationFromReports informationFromReports = new InformationFromReports();
-        ArrayList<MonthlyReport> monthlyReports = new ArrayList();
-        YearlyReport yearlyReport = new YearlyReport();
+        ArrayList<MonthlyReport> monthlyReports = new ArrayList<>();
+        YearlyReport yearlyReport = null;
 
         while(true) {
-            while(true) {
-                printMenu();
-                int command = scanner.nextInt();
-                if (command == 1) {
-                    monthlyReports = reportReader.readAllMonthlyReports();
-                    System.out.println("Месячные отчёты считаны успешно!");
-                } else if (command == 2) {
-                    yearlyReport = reportReader.getYearlyReport();
-                    System.out.println("Годовой отчёт считан успешно!");
-                } else if (command == 3) {
-                    if (!monthlyReports.isEmpty() && yearlyReport != null) {
-                        reportCompare.compareMonthlyAndYearlyReports(yearlyReport, monthlyReports);
-                        System.out.println("Сверка отчётов успешно пройдена!");
-                    } else {
-                        System.out.println("Необходимо считать все отчёты перед их сверкой.");
-                    }
-                } else if (command == 4) {
-                    if (monthlyReports.isEmpty()) {
-                        System.out.println("Необходимо считать все отчёты перед их печатью!");
-                    } else {
-                        System.out.println("Информация из месячных отчётов:");
-                        informationFromReports.getInfoFromAllMonths(monthlyReports);
-                    }
-                } else if (command == 5) {
-                    if (yearlyReport == null) {
-                        System.out.println("Необходимо считать годовой отчёт перед печатью!");
-                    } else {
-                        System.out.println("Информация из годового отчёта");
-                        informationFromReports.getInfoFromTheAnnualReport(yearlyReport);
-                    }
+            printMenu();
+            int command = scanner.nextInt();
+            if (command == 1) {
+                monthlyReports = reportReader.readAllMonthlyReports();
+                System.out.println("Месячные отчёты считаны успешно!");
+            } else if (command == 2) {
+                yearlyReport = reportReader.getYearlyReport();
+                System.out.println("Годовой отчёт считан успешно!");
+            } else if (command == 3) {
+                if (!monthlyReports.isEmpty() && yearlyReport != null) {
+                    reportCompare.compareMonthlyAndYearlyReports(yearlyReport, monthlyReports);
+                    System.out.println("Сверка отчётов успешно пройдена!");
                 } else {
-                    if (command == 0) {
-                        System.out.println("Выйти из приложения");
-                        return;
-                    }
-
-                    System.out.println("Такой команды не существует, просьба указать команду повторно:");
+                    System.out.println("Необходимо считать все отчёты перед их сверкой.");
                 }
+            } else if (command == 4) {
+                if (monthlyReports.isEmpty()) {
+                    System.out.println("Необходимо считать все отчёты перед их печатью!");
+                } else {
+                    System.out.println("Информация из месячных отчётов:");
+                    informationFromReports.getInfoFromAllMonths(monthlyReports);
+                }
+            } else if (command == 5) {
+                if (yearlyReport == null) {
+                    System.out.println("Необходимо считать годовой отчёт перед печатью!");
+                } else {
+                    System.out.println("Информация из годового отчёта");
+                    informationFromReports.getInfoFromTheAnnualReport(yearlyReport);
+                }
+            } else if (command == 0) {
+                System.out.println("Выйти из приложения");
+                return;
+            } else {
+                System.out.println("Такой команды не существует, просьба указать команду повторно:");
             }
         }
     }
